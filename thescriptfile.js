1f198b98db51409c66e401a4209b095339f6faad
Nick Strupat <nick.strupat@gmail.com>
2016-03-04 04:16:07 -0500

    commit the compiled js and css for "your graciousness" github.io to host

---
+++ thescriptfile.js
@@ -0,0 +1,32 @@
+function updateTime() {
+    var date = new Date();
+    document.getElementById('time').innerHTML = addZero(date.getHours()) + ":" + addZero(date.getMinutes()) + ":" + addZero(date.getSeconds());
+    setTimeout(updateTime, 1000);
+}
+function addZero(i) { return i < 10 ? "0" + i : i; }
+function init() {
+    updateTime();
+    var date = new Date();
+    document.getElementById('date').innerHTML = (date.getFullYear() - 2000) + "." + (date.getMonth() + 1) + "." + date.getDate();
+    document.getElementById('page-length').innerHTML = document.documentElement.innerHTML.length.toString();
+    var emailAnchor = document.querySelector('a.email');
+    var buttonAnchor = document.querySelector('button.email');
+    if (document.queryCommandSupported("copy") === true) {
+        var $parent = emailAnchor.parentElement;
+        $parent.removeChild(emailAnchor);
+        buttonAnchor.style.display = 'inline-block';
+        buttonAnchor.addEventListener('click', function (event) {
+            copyEmailToClipboard(emailAnchor.innerHTML);
+        });
+    }
+}
+function copyEmailToClipboard(text) {
+    var $textArea = document.createElement('textarea');
+    $textArea.value = text;
+    document.body.appendChild($textArea);
+    $textArea.select();
+    var success = document.execCommand('copy');
+    document.body.removeChild($textArea);
+    if (success !== true)
+        alert('Could not copy email to clipboard. Try selecting the text and copying like the old days.');
+}
